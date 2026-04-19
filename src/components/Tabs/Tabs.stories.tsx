56743514a3d361ea9141ff19395634dc39e5b90f
ashes1452 <16416236+ashes1452@user.noreply.gitee.com>
2026-04-19 14:19:05 +0800

    add deploy storybook workflow

--- src/components/Tabs/Tabs.stories.tsx
+++ src/components/Tabs/Tabs.stories.tsx
@@ -12,10 +12,10 @@ type Story = StoryObj<typeof Tabs>
 export const 基础标签页: Story = {
     args: {
         items: [
-            { label: "111", key: "111" },
-            { label: "222", key: "222" },
-            { label: "333", key: "333", disabled: true },
-            { label: "444", key: "444" },
+            { label: "111", key: "111" ,content:<div>111内容</div>},
+            { label: "222", key: "222" ,content:<div>222内容</div>},
+            { label: "333", key: "333", disabled: true ,content:<div>333内容</div>},
+            { label: "444", key: "444" ,content:<div>444内容</div>},
         ],
     },
 }
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -5,6 +5,7 @@ export type TabItem = {
     label: string
     key: string
     disabled?: boolean
+    content: React.ReactNode 
 }
 
 export interface TabsProps {
@@ -28,20 +29,31 @@ const Tabs: React.FC<TabsProps> = ({
         onChange?.(key)
     }
 
+    // 找到当前选中的内容
+    const activeContent = items.find(item => item.key === activeKey)?.content
+
     return (
-        <div className="my-tabs">
-            {items.map((item) => (
-                <div
-                    key={item.key}
-                    className={`my-tab
-            ${activeKey === item.key ? "my-tab-active" : ""}
-            ${item.disabled ? "my-tab-disabled" : ""}
-          `}
-                    onClick={() => handleClick(item.key, item.disabled)}
-                >
-                    {item.label}
-                </div>
-            ))}
+        <div>
+            {/* 标签栏 */}
+            <div className="my-tabs">
+                {items.map((item) => (
+                    <div
+                        key={item.key}
+                        className={`my-tab
+                            ${activeKey === item.key ? "my-tab-active" : ""}
+                            ${item.disabled ? "my-tab-disabled" : ""}
+                        `}
+                        onClick={() => handleClick(item.key, item.disabled)}
+                    >
+                        {item.label}
+                    </div>
+                ))}
+            </div>
+
+            {/* 内容区域 */}
+            <div className="my-tab-content">
+                {activeContent}
+            </div>
         </div>
     )
 }
